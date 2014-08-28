02ae840045cb8c8d5168c3c48b3a3f446ab7d44a
Jay Mundrawala <jdmundrawala@gmail.com>
2014-08-28 16:34:08 -0700

    Bootstrap allows powershell version and allows running only chef client

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,19 +1,36 @@
 Param(
+    [Parameter(Mandatory=$true)]
     [string]$NodeName,
     [string]$GithubOwner = "opscode",
     [string]$GithubRepo = "chef",
-    [string]$GitRevision = "platform/dsc-phase-1"
+    [string]$GitRevision = "platform/dsc-phase-1",
+    [string]$PSVersion = 4,
+    [switch]$ClientRunOnly
 )
 
-$attributes = @"
-{
-    \"dsc_test_bootstrap\": {
-        \"github_owner\": \"$GithubOwner\",
-        \"github_repo\": \"$GithubRepo\",
-        \"git_revision\": \"$GitRevision\"
+if ($ClientRunOnly.IsPresent) {
+    knife winrm "name:$NodeName" "chef-client" -a cloud.public_fqdn
+} else {
+    $attributes = @"
+    {
+        \"dsc_test_bootstrap\": {
+            \"github_owner\": \"$GithubOwner\",
+            \"github_repo\": \"$GithubRepo\",
+            \"git_revision\": \"$GitRevision\"
+        }
     }
-}
 "@ -replace "`n"," "
 
-#knife azure server create --azure-dns-name $NodeName --run-list `"recipe[dsc-test-bootstrap]`" -j `"$attributes`"
-knife azure server create --azure-dns-name $NodeName --run-list `'recipe[dsc-test-bootstrap]`' -j `"$attributes`"
+    $recipes = @()
+
+    switch ($PSVersion) {
+        4 {$recipes += 'recipe[powershell::powershell4]'}
+        5 {$recipes += 'recipe[powershell::powershell5]'}
+        default {Throw "Unsupported powershell version"}
+    }
+
+    $recipes += @('recipe[dsc-test-bootstrap]')
+    $recipes =  $recipes -join(',')
+
+    knife azure server create --azure-dns-name $NodeName --run-list `'$recipes`' -j `"$attributes`"
+}
